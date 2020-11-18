92f0a0a483576406bc2984f9f28e3b1c4e9b79a6
jianghuafei <jianghuafei12@163.com>
2020-11-18 16:55:59 +0800

    [新增]新增动态表单

--- flance-components/flance-components-form/src/main/java/com/flance/components/form/domain/dform/repository/FlanceFormRecMainflowformDao.java
+++ flance-components/flance-components-form/src/main/java/com/flance/components/form/domain/dform/repository/FlanceFormRecMainflowformDao.java
@@ -1,9 +1,9 @@
 package com.flance.components.form.domain.dform.repository;
 
-import com.flance.components.form.domain.dform.service.FlanceFormRecMainflowformService;
+import com.flance.components.form.domain.dform.model.po.FlanceFormRecMainflowform;
 import com.flance.jdbc.jpa.dao.BaseDao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FlanceFormRecMainflowformDao extends BaseDao<FlanceFormRecMainflowformService, String> {
+public interface FlanceFormRecMainflowformDao extends BaseDao<FlanceFormRecMainflowform, String> {
 }
