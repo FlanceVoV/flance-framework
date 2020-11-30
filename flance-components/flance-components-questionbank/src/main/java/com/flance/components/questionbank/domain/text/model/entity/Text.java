1e01833575d4e5a1f2ca8617ab6b390c7e74f4c9
jianghuafei <jianghuafei12@163.com>
2020-11-30 10:45:22 +0800

    [新增]新增题库配置

--- flance-components/flance-components-questionbank/src/main/java/com/flance/components/questionbank/domain/text/model/entity/Text.java
+++ flance-components/flance-components-questionbank/src/main/java/com/flance/components/questionbank/domain/text/model/entity/Text.java
@@ -2,15 +2,21 @@ package com.flance.components.questionbank.domain.text.model.entity;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Table;
 
 /**
  * 大段文本，提取出来，防止影响jpa查询效率
  * @author jhf
  */
 @Data
+@Entity
+@Table(name = "F_COMP_QB_Q_TEXT")
 public class Text {
 
+    @Id
     private Long id;
 
     @Lob
