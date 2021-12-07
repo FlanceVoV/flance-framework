e2eaa8eb8f870ebce2dd69b7d84d2e0c18027d06
jianghf41372 <18151681814Jhf>
2021-12-07 10:00:05 +0800

    [修改]全局响应拦截

--- flance-web/flance-web-utils/src/main/java/com/flance/web/utils/config/GobalResponseBodyAdvice.java
+++ flance-web/flance-web-utils/src/main/java/com/flance/web/utils/config/GobalResponseBodyAdvice.java
@@ -7,10 +7,12 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.util.List;
 
+@ControllerAdvice
 public class GobalResponseBodyAdvice implements ResponseBodyAdvice {
 
     private List<String> ignoreUrl;
