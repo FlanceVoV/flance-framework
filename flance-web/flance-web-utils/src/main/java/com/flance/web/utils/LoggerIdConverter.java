bbae8d4d59fa1524635db2d47fba4e3aead47e1f
jianghf41372 <18151681814Jhf>
2021-12-07 10:43:34 +0800

    [修改]全局响应拦截

---
+++ flance-web/flance-web-utils/src/main/java/com/flance/web/utils/LoggerIdConverter.java
@@ -0,0 +1,15 @@
+package com.flance.web.utils;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class LoggerIdConverter extends ClassicConverter {
+
+    public LoggerIdConverter() {
+    }
+
+    @Override
+    public String convert(ILoggingEvent iLoggingEvent) {
+        return RequestUtil.getLogId();
+    }
+}
