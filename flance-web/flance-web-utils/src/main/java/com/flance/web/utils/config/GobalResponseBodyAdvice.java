1e086c8c3745029f36715a2e4cdfc78c83a3bd9e
jianghf41372 <18151681814Jhf>
2021-12-07 10:20:28 +0800

    [修改]全局响应拦截

--- flance-web/flance-web-utils/src/main/java/com/flance/web/utils/config/GobalResponseBodyAdvice.java
+++ flance-web/flance-web-utils/src/main/java/com/flance/web/utils/config/GobalResponseBodyAdvice.java
@@ -1,11 +1,14 @@
 package com.flance.web.utils.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flance.web.utils.UrlMatchUtil;
 import com.flance.web.utils.web.response.WebResponse;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,28 +17,35 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import java.util.List;
 
 @ControllerAdvice
-public class GobalResponseBodyAdvice implements ResponseBodyAdvice {
+public class GobalResponseBodyAdvice implements ResponseBodyAdvice<Object> {
 
     @Value("${flance.response.advice::#{null}}")
     private List<String> ignoreUrl;
 
     @Override
-    public boolean supports(MethodParameter methodParameter, Class aClass) {
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> clazz) {
         return true;
     }
 
+    @SneakyThrows
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter methodParameter,
-                                  MediaType mediaType, Class aClass,
+                                  MediaType mediaType, Class<? extends HttpMessageConverter<?>> clazz,
                                   ServerHttpRequest request,
                                   ServerHttpResponse response) {
+        Object result;
 
         if (body instanceof WebResponse ||
                 ignore(request.getURI().toString())) {
-            return body;
+            result = body;
+        } else if (body instanceof String) {
+            ObjectMapper mapper = new ObjectMapper();
+            result = mapper.writeValueAsString(WebResponse.getSucceed(body, "请求成功"));
+        } else {
+            result = WebResponse.getSucceed(body, "请求成功");
         }
 
-        return WebResponse.getSucceed(body, "请求成功");
+        return result;
     }
 
     private boolean ignore(String url) {
