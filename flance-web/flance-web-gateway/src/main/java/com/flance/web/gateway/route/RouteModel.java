104e8cbd5355ec5497dd2b555e57fdd48fe3359b
jianghf41372 <18151681814Jhf>
2022-01-17 16:41:51 +0800

    [修改]增加分布式服务全局日志id

--- flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/route/RouteModel.java
+++ flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/route/RouteModel.java
@@ -41,4 +41,16 @@ public interface RouteModel {
      * @return  返回过滤器
      */
     String getFilter();
+
+    /**
+     * 获取全局限流配置 单位： 请求次数/毫秒
+     * @return  返回限流
+     */
+    Integer getLimit();
+
+    /**
+     * 获取接口超时配置 单位：ms
+     * @return  返回超时
+     */
+    Integer getTimeOut();
 }
