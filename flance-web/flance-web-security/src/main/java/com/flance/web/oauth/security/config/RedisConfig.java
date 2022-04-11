b968eafce45b352215a536e91110b029983a837d
jianghf41372 <18151681814Jhf>
2022-04-11 15:01:28 +0800

    [修改]fix

--- flance-web/flance-web-security/src/main/java/com/flance/web/oauth/security/config/RedisConfig.java
+++ flance-web/flance-web-security/src/main/java/com/flance/web/oauth/security/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import javax.annotation.Resource;
@@ -21,8 +22,11 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
