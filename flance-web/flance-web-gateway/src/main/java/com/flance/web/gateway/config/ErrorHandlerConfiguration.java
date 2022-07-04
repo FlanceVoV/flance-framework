2e9de44a0868e87a2604b4885e7d126f3657db72
jianghf41372 <18151681814Jhf>
2022-07-04 16:20:00 +0800

    [新增]springboot 2.6版本升级

--- flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/config/ErrorHandlerConfiguration.java
+++ flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/config/ErrorHandlerConfiguration.java
@@ -1,65 +1,66 @@
-package com.flance.web.gateway.config;
-
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.result.view.ViewResolver;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.web.reactive.error.ErrorAttributes;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.codec.ServerCodecConfigurer;
-
-import java.util.Collections;
-import java.util.List;
-
-/**
- * 网关异常处理配置
- * @author jhf
- */
-@Configuration
-@EnableConfigurationProperties({ServerProperties.class, ResourceProperties.class})
-public class ErrorHandlerConfiguration {
-
-    private final ServerProperties serverProperties;
-
-    private final ApplicationContext applicationContext;
-
-    private final ResourceProperties resourceProperties;
-
-    private final List<ViewResolver> viewResolvers;
-
-    private final ServerCodecConfigurer serverCodecConfigurer;
-
-
-    public ErrorHandlerConfiguration(ServerProperties serverProperties,
-                                     ResourceProperties resourceProperties,
-                                     ObjectProvider<List<ViewResolver>> viewResolversProvider,
-                                     ServerCodecConfigurer serverCodecConfigurer,
-                                     ApplicationContext applicationContext) {
-        this.serverProperties = serverProperties;
-        this.applicationContext = applicationContext;
-        this.resourceProperties = resourceProperties;
-        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
-        this.serverCodecConfigurer = serverCodecConfigurer;
-    }
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public ErrorWebExceptionHandler errorWebExceptionHandler(ErrorAttributes errorAttributes) {
-        GatewayExceptionHandler exceptionHandler = new GatewayExceptionHandler(
-                errorAttributes,
-                this.resourceProperties,
-                this.serverProperties.getError(),
-                this.applicationContext);
-        exceptionHandler.setViewResolvers(this.viewResolvers);
-        exceptionHandler.setMessageWriters(this.serverCodecConfigurer.getWriters());
-        exceptionHandler.setMessageReaders(this.serverCodecConfigurer.getReaders());
-        return exceptionHandler;
-    }
-
-}
+//package com.flance.web.gateway.config;
+//
+//import org.springframework.boot.autoconfigure.web.ResourceProperties;
+//import org.springframework.boot.autoconfigure.web.ServerProperties;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.result.view.ViewResolver;
+//import org.springframework.beans.factory.ObjectProvider;
+//import org.springframework.boot.web.reactive.error.ErrorAttributes;
+//import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.core.Ordered;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.http.codec.ServerCodecConfigurer;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+///**
+// * 网关异常处理配置
+// * @author jhf
+// */
+//@Deprecated
+//@Configuration
+//@EnableConfigurationProperties({ServerProperties.class, ResourceProperties.class})
+//public class ErrorHandlerConfiguration {
+//
+//    private final ServerProperties serverProperties;
+//
+//    private final ApplicationContext applicationContext;
+//
+//    private final ResourceProperties resourceProperties;
+//
+//    private final List<ViewResolver> viewResolvers;
+//
+//    private final ServerCodecConfigurer serverCodecConfigurer;
+//
+//
+//    public ErrorHandlerConfiguration(ServerProperties serverProperties,
+//                                     ResourceProperties resourceProperties,
+//                                     ObjectProvider<List<ViewResolver>> viewResolversProvider,
+//                                     ServerCodecConfigurer serverCodecConfigurer,
+//                                     ApplicationContext applicationContext) {
+//        this.serverProperties = serverProperties;
+//        this.applicationContext = applicationContext;
+//        this.resourceProperties = resourceProperties;
+//        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+//        this.serverCodecConfigurer = serverCodecConfigurer;
+//    }
+//
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public ErrorWebExceptionHandler errorWebExceptionHandler(ErrorAttributes errorAttributes) {
+//        GatewayExceptionHandler exceptionHandler = new GatewayExceptionHandler(
+//                errorAttributes,
+//                this.resourceProperties,
+//                this.serverProperties.getError(),
+//                this.applicationContext);
+//        exceptionHandler.setViewResolvers(this.viewResolvers);
+//        exceptionHandler.setMessageWriters(this.serverCodecConfigurer.getWriters());
+//        exceptionHandler.setMessageReaders(this.serverCodecConfigurer.getReaders());
+//        return exceptionHandler;
+//    }
+//
+//}
--- flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/config/GatewayExceptionHandler.java
+++ flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/config/GatewayExceptionHandler.java
@@ -1,113 +1,114 @@
-package com.flance.web.gateway.config;
-
-import com.flance.web.utils.AssertException;
-import com.flance.web.utils.RequestUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.web.ErrorProperties;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
-import org.springframework.boot.web.reactive.error.ErrorAttributes;
-import org.springframework.context.ApplicationContext;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * 网关异常处理
- * @author jhf
- */
-@Slf4j
-public class GatewayExceptionHandler extends DefaultErrorWebExceptionHandler {
-
-    public GatewayExceptionHandler(ErrorAttributes errorAttributes, ResourceProperties resourceProperties, ErrorProperties errorProperties, ApplicationContext applicationContext) {
-        super(errorAttributes, resourceProperties, errorProperties, applicationContext);
-    }
-
-
-    /**
-     * 获取异常属性
-     */
-    @Override
-    protected Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
-        String code = "500";
-        String msg = "";
-        Throwable error = super.getError(request);
-        if (error instanceof org.springframework.cloud.gateway.support.NotFoundException) {
-            code = "404";
-            msg = "找不到服务[" + error.getMessage() + "]";
-        }
-        if (error instanceof AssertException) {
-            code = ((AssertException) error).getCode();
-            msg = ((AssertException) error).getMsg();
-        }
-        log.error("清除threadLocal");
-        RequestUtil.remove();
-        return response(code + "", msg);
-    }
-
-    /**
-     * 指定响应处理方法为JSON处理的方法
-     * @param errorAttributes
-     */
-    @Override
-    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
-        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
-    }
-
-    /**
-     * 根据code获取对应的HttpStatus
-     * @param errorAttributes
-     * @return
-     */
-    @Override
-    protected int getHttpStatus(Map<String, Object> errorAttributes) {
-        Object errorCode = errorAttributes.get("code");
-        try {
-            if (errorCode.toString().length() == 3) {
-                return Integer.parseInt(errorCode.toString());
-            } else {
-                return 500;
-            }
-        } catch (Exception e) {
-            return 500;
-        }
-    }
-
-    public static Map<String, Object> response(String status, String errorMsg) {
-        Map<String, Object> map = new HashMap<String, Object>(3){{
-            put("code", status);
-            put("msg", errorMsg);
-            put("data", null);
-            put("success", false);
-        }};
-
-        return map;
-    }
-
-    /**
-     * 构建异常信息
-     * @param request
-     * @param ex
-     * @return
-     */
-    private String buildMessage(ServerRequest request, Throwable ex) {
-        StringBuilder message = new StringBuilder("Failed to handle request [");
-        message.append(request.methodName());
-        message.append(" ");
-        message.append(request.uri());
-        message.append("]");
-        if (ex != null) {
-            message.append(": ");
-            message.append(ex.getMessage());
-        }
-        return message.toString();
-    }
-
-}
+//package com.flance.web.gateway.config;
+//
+//import com.flance.web.utils.AssertException;
+//import com.flance.web.utils.RequestUtil;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.autoconfigure.web.ErrorProperties;
+//import org.springframework.boot.autoconfigure.web.ResourceProperties;
+//import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
+//import org.springframework.boot.web.reactive.error.ErrorAttributes;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.reactive.function.server.ServerRequest;
+//import org.springframework.web.reactive.function.server.RequestPredicates;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.RouterFunctions;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//import reactor.core.publisher.Mono;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * 网关异常处理
+// * @author jhf
+// */
+//@Slf4j
+//@Deprecated
+//public class GatewayExceptionHandler extends DefaultErrorWebExceptionHandler {
+//
+//    public GatewayExceptionHandler(ErrorAttributes errorAttributes, ResourceProperties resourceProperties, ErrorProperties errorProperties, ApplicationContext applicationContext) {
+//        super(errorAttributes, resourceProperties, errorProperties, applicationContext);
+//    }
+//
+//
+//    /**
+//     * 获取异常属性
+//     */
+//    @Override
+//    protected Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
+//        String code = "500";
+//        String msg = "";
+//        Throwable error = super.getError(request);
+//        if (error instanceof org.springframework.cloud.gateway.support.NotFoundException) {
+//            code = "404";
+//            msg = "找不到服务[" + error.getMessage() + "]";
+//        }
+//        if (error instanceof AssertException) {
+//            code = ((AssertException) error).getCode();
+//            msg = ((AssertException) error).getMsg();
+//        }
+//        log.error("清除threadLocal");
+//        RequestUtil.remove();
+//        return response(code + "", msg);
+//    }
+//
+//    /**
+//     * 指定响应处理方法为JSON处理的方法
+//     * @param errorAttributes
+//     */
+//    @Override
+//    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+//        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+//    }
+//
+//    /**
+//     * 根据code获取对应的HttpStatus
+//     * @param errorAttributes
+//     * @return
+//     */
+//    @Override
+//    protected int getHttpStatus(Map<String, Object> errorAttributes) {
+//        Object errorCode = errorAttributes.get("code");
+//        try {
+//            if (errorCode.toString().length() == 3) {
+//                return Integer.parseInt(errorCode.toString());
+//            } else {
+//                return 500;
+//            }
+//        } catch (Exception e) {
+//            return 500;
+//        }
+//    }
+//
+//    public static Map<String, Object> response(String status, String errorMsg) {
+//        Map<String, Object> map = new HashMap<String, Object>(3){{
+//            put("code", status);
+//            put("msg", errorMsg);
+//            put("data", null);
+//            put("success", false);
+//        }};
+//
+//        return map;
+//    }
+//
+//    /**
+//     * 构建异常信息
+//     * @param request
+//     * @param ex
+//     * @return
+//     */
+//    private String buildMessage(ServerRequest request, Throwable ex) {
+//        StringBuilder message = new StringBuilder("Failed to handle request [");
+//        message.append(request.methodName());
+//        message.append(" ");
+//        message.append(request.uri());
+//        message.append("]");
+//        if (ex != null) {
+//            message.append(": ");
+//            message.append(ex.getMessage());
+//        }
+//        return message.toString();
+//    }
+//
+//}
