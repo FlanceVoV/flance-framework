028f202e9686ee95f9ea4cf36b2ebb087daedae5
jiang <809741492@qq.com>
2023-06-14 14:16:23 +0800

    [修改]

--- flance-tx/flance-tx-client/src/main/java/com/flance/tx/client/netty/biz/ClientPongReceiverHandler.java
+++ flance-tx/flance-tx-client/src/main/java/com/flance/tx/client/netty/biz/ClientPongReceiverHandler.java
@@ -5,22 +5,17 @@ import com.flance.tx.netty.data.NettyRequest;
 import com.flance.tx.netty.data.NettyResponse;
 import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@ConditionalOnMissingBean
 @Component("clientPongReceiverHandler")
 public class ClientPongReceiverHandler implements IBizHandler<NettyRequest, NettyResponse> {
 
     @Override
     public NettyRequest doBizHandler(NettyResponse response, Channel channel) {
         log.info("get pong success");
-        NettyRequest request = new NettyRequest();
-        request.setHandlerId("clientStatusHandler");
-        request.setRoomId(response.getRoomId());
-        request.setData("客户端信息json");
-        request.setMessageId(response.getMessageId());
-        request.setServerData(response.getServerData());
-        request.setClientId(response.getClientId());
-        return request;
+        return null;
     }
 }
