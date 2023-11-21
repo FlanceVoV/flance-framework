8bc2ad927bab57bf10668c4eadd5d2fa1dd69b3f
jiang <809741492@qq.com>
2023-11-21 18:10:06 +0800

    [修改]bug fix

--- flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/filter/GlobalGatewayFilter.java
+++ flance-web/flance-web-gateway/src/main/java/com/flance/web/gateway/filter/GlobalGatewayFilter.java
@@ -17,7 +17,9 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
+import java.net.InetAddress;
 import java.net.URLEncoder;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
@@ -40,13 +42,14 @@ public class GlobalGatewayFilter implements GlobalFilter, Ordered {
         String setLogId = RequestUtil.getLogId(headerLogId);
         String appId = exchange.getRequest().getHeaders().getFirst(RequestConstant.HEADER_APP_ID);
         String headerChain = exchange.getRequest().getHeaders().getFirst(RequestConstant.HEADER_REQUEST_CHAIN);
+        String ip = getIpAddress(exchange);
         String logId = UUID.randomUUID().toString();
         RequestUtil.setLogId(logId);
         if (!StringUtils.hasLength(requestId)) {
             requestId = uri;
         }
         String requestChain = null == headerChain ? requestId : headerChain.replaceAll("\\[", "").replaceAll("]", "");
-        log.info("gateway-global-filter：请求路径[({}){}]，url标识[{}]", method, uri, requestId);
+        log.info("gateway-global-filter：IP：[{}]  请求路径[({}){}]，url标识[{}]", ip, method, uri, requestId);
         final String token = exchange.getRequest().getHeaders().getFirst(RequestConstant.HEADER_TOKEN);
 
         // 设置下游传递参数
@@ -75,5 +78,46 @@ public class GlobalGatewayFilter implements GlobalFilter, Ordered {
     @Override
     public int getOrder() {
         return HIGHEST_PRECEDENCE + 1;
+    }
+
+
+    private static String getIpAddress(ServerWebExchange exchange) {
+        String ip = exchange.getRequest().getHeaders().getFirst("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = exchange.getRequest().getHeaders().getFirst("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = exchange.getRequest().getHeaders().getFirst("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = exchange.getRequest().getHeaders().getFirst("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = exchange.getRequest().getHeaders().getFirst("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = exchange.getRequest().getHeaders().getFirst("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = "127.0.0.1";
+            log.info(ip);
+            if (ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1")) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) { //"***.***.***.***".length() = 15
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
     }
 }
